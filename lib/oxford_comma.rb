6ee23398f32fc75ce99b7a1206c77937b106189a
Joan in DC <joanrigdon@gmail.com>
2019-01-31 18:46:52 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -11,7 +11,7 @@ def oxford_comma(array)
   
   if array.length > 2
     last = array.pop
-    return array.join(", ") + " and #{array.pop}"
+    return array.join(", ") + ", and #{array.pop}"
     binding.pry
   end
 end
