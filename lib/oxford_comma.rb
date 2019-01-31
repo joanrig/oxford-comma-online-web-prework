47846fbea281b3842c636ac6518ac0833335f2a1
Joan in DC <joanrigdon@gmail.com>
2019-01-31 18:42:38 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,16 +1,29 @@
 def oxford_comma(array)
-  
-  if 
-    array.length == 1 
-    array.shift
-  elsif 
-    array.length == 2   
-    array.join (" and ")
-  elsif 
-    array.length > 2
-      last = array.pop
-      string = array.join(", ")
-      return string + ", and " + last
+  if array.length == 1 
+    return array.join
   end
   
+  if array.length == 2 
+    return array.join(" and ")
+  end
+  
+  if array.length == 3
+    return "#{arr"
 end
+
+# def oxford_comma(array)
+  
+#   if 
+#     array.length == 1 
+#     array.shift
+#   elsif 
+#     array.length == 2   
+#     array.join (" and ")
+#   elsif 
+#     array.length > 2
+#       last = array.pop
+#       string = array.join(", ")
+#       return string + ", and " + last
+#   end
+  
+# end
